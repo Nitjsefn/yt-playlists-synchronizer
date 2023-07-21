874b2d924f77008a797391f7673ecd526aaf89aa
Nitjsefn <>
2023-07-21 23:50:22 +0200

    added usings into Program.cs and changed namespace into old type

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
-﻿namespace yt_playlists_synchronizer;
-class Program
+﻿using System;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace yt_playlists_synchronizer
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
     }
 }
