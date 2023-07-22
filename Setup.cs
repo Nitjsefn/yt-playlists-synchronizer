28523e317ada68471afaa2cf4906ca19ad4571e2
Nitjsefn <>
2023-07-23 00:37:16 +0200

    added reading basic configuration file in Setup constructor

--- Setup.cs
+++ Setup.cs
@@ -13,8 +13,29 @@ namespace yt_playlists_synchronizer
     class Setup
     {
         private string Token;
-        public string PLsToSyncFilename;
+        public string PLsToSyncPath;
         public List<PlaylistToSync> PLsToSync { get; }
-
+        public Setup(string configFilename)
+        {
+            Token = "";
+            PLsToSyncPath = "";
+            if(!File.Exists(configFilename)) throw new FileNotFoundException("Could not find configuration file.");
+            string cfgFile = File.ReadAllText(configFilename);
+            int lineStartIndx = cfgFile.IndexOf("Token:");
+            if(lineStartIndx == -1) throw new Exception("Configuration file is invalid. Could not find Token.");
+            for(int i = 7 + lineStartIndx; i < cfgFile.Length && cfgFile[i] != ';'; i++)
+            {
+                Token += cfgFile[i];
+            }
+            if(Token.Length == 0) throw new Exception("Configuration file is invalid. Could not find Token.");
+            lineStartIndx = cfgFile.IndexOf("PLsToSyncPath:");
+            if(lineStartIndx == -1) throw new Exception("Configuration file is invalid. Could not find path to file with playlists to sync.");
+            for(int i = 15 + lineStartIndx; i < cfgFile.Length && cfgFile[i] != ';'; i++)
+            {
+                PLsToSyncPath += cfgFile[i];
+            }
+            if(PLsToSyncPath.Length == 0) throw new Exception("Configuration file is invalid. Could not find path to file with playlists to sync.");
+            Console.WriteLine(PLsToSyncPath);
+        }
     }
 }
