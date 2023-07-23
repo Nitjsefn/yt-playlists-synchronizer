c2ae3d705c7e6bab52ca693c0178e40057e6c5e7
Nitjsefn <>
2023-07-24 00:03:32 +0200

    added reading PLsToSync configuration file in Setup.cs contructor

--- Setup.cs
+++ Setup.cs
@@ -6,10 +6,11 @@ namespace yt_playlists_synchronizer
 {
     struct PlaylistToSync
     {
-        string DesiredPlaylistName;
-        string PlaylistID;
-        int NumberingOffset;
+        public string DesiredPlaylistName;
+        public string PlaylistID;
+        public int NumberingOffset;
     }
+
     class Setup
     {
         private string Token;
@@ -19,23 +20,51 @@ namespace yt_playlists_synchronizer
         {
             Token = "";
             PLsToSyncPath = "";
-            if(!File.Exists(configFilename)) throw new FileNotFoundException("Could not find configuration file.");
+			PLsToSync = new List<PlaylistToSync>();
+			//New method ReadCOnfigFile
+            if(!File.Exists(configFilename))
+				throw new FileNotFoundException("Could not find configuration file.");
             string cfgFile = File.ReadAllText(configFilename);
             int lineStartIndx = cfgFile.IndexOf("Token:");
-            if(lineStartIndx == -1) throw new Exception("Configuration file is invalid. Could not find Token.");
+            if(lineStartIndx == -1)
+				throw new Exception("Configuration file is invalid. Could not find Token.");
             for(int i = 7 + lineStartIndx; i < cfgFile.Length && cfgFile[i] != ';'; i++)
             {
                 Token += cfgFile[i];
             }
-            if(Token.Length == 0) throw new Exception("Configuration file is invalid. Could not find Token.");
+            if(Token.Length == 0)
+				throw new Exception("Configuration file is invalid. Could not find Token.");
             lineStartIndx = cfgFile.IndexOf("PLsToSyncPath:");
-            if(lineStartIndx == -1) throw new Exception("Configuration file is invalid. Could not find path to file with playlists to sync.");
+            if(lineStartIndx == -1)
+				throw new Exception("Configuration file is invalid. Could not find path to file with playlists to sync.");
             for(int i = 15 + lineStartIndx; i < cfgFile.Length && cfgFile[i] != ';'; i++)
             {
                 PLsToSyncPath += cfgFile[i];
             }
-            if(PLsToSyncPath.Length == 0) throw new Exception("Configuration file is invalid. Could not find path to file with playlists to sync.");
-            Console.WriteLine(PLsToSyncPath);
+            if(PLsToSyncPath.Length == 0)
+				throw new Exception("Configuration file is invalid. Could not find path to file with playlists to sync.");
+
+            if(!File.Exists(PLsToSyncPath))
+				throw new FileNotFoundException("Could not find playlists configuration file.");
+            string[] playlistsFileLines = File.ReadAllLines(PLsToSyncPath);
+			//new method ReadPlToSyncFile
+			foreach(var line in playlistsFileLines)
+			{
+				//For each line call method GetPlaylistSyncInfo
+				PlaylistToSync playlist = new PlaylistToSync();
+				string[] splitted;
+				splitted = line.Split(' ');
+				//Add checking if name and id fields are present
+				playlist.DesiredPlaylistName = splitted[0];
+				playlist.PlaylistID = splitted[1];
+				int numberingOffset;
+				if(splitted.Length == 3)
+					if(int.TryParse(splitted[2], out numberingOffset))
+						playlist.NumberingOffset = numberingOffset;
+				PLsToSync.Add(playlist);
+			}
+			foreach(var pl in PLsToSync)
+				Console.WriteLine(pl.PlaylistID + " " + pl.DesiredPlaylistName + " " + pl.NumberingOffset);
         }
     }
 }
